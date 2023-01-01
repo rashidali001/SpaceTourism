4de5fa72a9cab77de49f88b1c0a03dc82a820b91
rashidali001 <rashidestonian@gmail.com>
2023-01-01 17:47:39 +0300

    portfolio

--- starter-code/scripts/crew.js
+++ starter-code/scripts/crew.js
@@ -1,3 +1,37 @@
 import data from '../data.json' assert {type: 'json'};
 
 const crew = data.crew // array of objects
+
+let change = document.querySelectorAll('div.change');
+let crew_img = document.querySelector('img.crew_img');
+let crew_no = 0;
+
+
+crew_img.src = crew[crew_no].images.png;
+
+/* change */ 
+for (let i = 0; i < change.length; i++)
+{
+    change[i].onclick = changeImg;
+}
+
+
+function changeImg()
+{
+    crew_no = this.id;
+    switch(crew_no)
+    {
+        case '0':
+            crew_img.src = crew[crew_no].images.png;
+            break;
+        case '1':
+            crew_img.src = crew[crew_no].images.png;
+            break;
+        case '2':
+            crew_img.src = crew[crew_no].images.png;
+            break;
+        case '3':
+            crew_img.src = crew[crew_no].images.png;
+            break;
+    }
+}
