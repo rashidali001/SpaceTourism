054e84786d3f28363577224db79a30a29b8b178c
rashidali001 <rashidestonian@gmail.com>
2023-01-01 13:57:52 +0300

    portfolio

--- starter-code/scripts/destination.js
+++ starter-code/scripts/destination.js
@@ -6,13 +6,21 @@ let planet_img = document.querySelector('div.planet_img');
 let planet_links = document.querySelector('div.planet_links');
 let planet_heading = document.querySelector('div.planet_heading');
 let planet_description = document.querySelector('div.planet_description');
+let distance = document.querySelector('div.distance');
+let time = document.querySelector('div.time');
+
 let img_no = 0;
 let heading_no = 0;
 let planet_description_no = 0;
+let distance_no = 0;
+let time_no = 0;
 
 displayImg();
 displayheading();
 displayInfo();
+displayDistance();
+displayTime();
+
 
 function displayImg()
 {
@@ -32,6 +40,25 @@ function displayInfo()
 {
     planet_description.innerHTML = destination[planet_description_no].description;
 }
+
+function displayDistance()
+{
+    let para = document.createElement('p');
+    let para_text = document.createTextNode(destination[distance_no].distance);
+    para.appendChild(para_text);
+    distance.appendChild(para);
+    para.classList.add('para');
+}
+
+function displayTime()
+{
+    let para = document.createElement('p');
+    let para_text = document.createTextNode(destination[time_no].travel);
+    para.appendChild(para_text);
+    time.appendChild(para);
+    para.classList.add('para');
+}
+
 
 
 /* displaying planet names */
