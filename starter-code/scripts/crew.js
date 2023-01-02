5386e73f36372a41608e569d342b24895151666d
rashidali001 <rashidestonian@gmail.com>
2023-01-02 15:26:30 +0300

    portfolio

--- starter-code/scripts/crew.js
+++ starter-code/scripts/crew.js
@@ -14,6 +14,8 @@ crew_img.src = crew[crew_no].images.png;
 crew_career.innerHTML = crew[crew_no].role;
 crew_name.innerHTML = crew[crew_no].name;
 crew_description.innerHTML = crew[crew_no].bio;
+let btn = document.getElementById('4').classList.add('active');
+let btn_small = document.getElementById('0').classList.add('active');
 
 /* change */ 
 for (let i = 0; i < change.length; i++)
@@ -29,28 +31,54 @@ function changeImg()
     {
         case '0':
             crew_img.src = crew[crew_no].images.png;
-            changeCrew();
+            changeCrew(this.id);
             break;
         case '1':
             crew_img.src = crew[crew_no].images.png;
-            changeCrew();
+            changeCrew(this.id);
             break;
         case '2':
             crew_img.src = crew[crew_no].images.png;
-            changeCrew();
+            changeCrew(this.id);
             break;
         case '3':
             crew_img.src = crew[crew_no].images.png;
-            changeCrew();
+            changeCrew(this.id);
+            break;
+        case '4':
+            crew_no -= 4;
+            crew_img.src = crew[crew_no].images.png;
+            changeCrew(this.id);
+            break;
+        case '5':
+            crew_no -= 4;
+            crew_img.src = crew[crew_no].images.png;
+            changeCrew(this.id);
+            break;
+        case '6':
+            crew_no -= 4;
+            crew_img.src = crew[crew_no].images.png;
+            changeCrew(this.id);
+            break;
+        case '7':
+            crew_no -= 4;
+            crew_img.src = crew[crew_no].images.png;
+            changeCrew(this.id);
             break;
     }
 }
 
-function changeCrew()
+function changeCrew(value)
 {
     crew_img.src = crew[crew_no].images.png;
     crew_career.innerHTML = crew[crew_no].role;
     crew_name.innerHTML = crew[crew_no].name;
     crew_description.innerHTML = crew[crew_no].bio;
 
+    let btn = document.getElementById(value);
+    for (let i = 0; i < change.length; i++)
+    {
+        change[i].classList.remove('active');
+    }
+    btn.classList.add('active');
 }
---
+++ starter-code/scripts/technology.js
@@ -0,0 +1,28 @@
+import data from '../data.json' assert {type : 'json'};
+
+
+const technology_array = data.technology;
+
+
+let tech_heading = document.querySelector('div.tech_heading');
+let tech_description = document.querySelector('div.tech_description');
+let tech_img = document.querySelector('img.tech_img');
+let tech_change = document.querySelectorAll('div.tech_change');
+
+let start = 0;
+
+tech_img.src = technology_array[start].images.portrait;
+tech_heading.innerHTML = technology_array[start].name;
+tech_description.innerHTML = technology_array[start].description;
+
+for ( let i = 0; i < tech_change.length; i++)
+{
+    tech_change[i].onclick = change_pic;
+}
+
+function change_pic()
+{
+    tech_img.src = technology_array[this.id].images.portrait;
+    tech_heading.innerHTML = technology_array[this.id].name;
+    tech_description.innerHTML = technology_array[this.id].description
+}
